dd37d6a1a62a79b97c3cb9736a7fd3529b6f5392
Victor <victor.catamo.rojas@gmail.com>
2019-04-01 20:13:53 -0300

    Deploy file is back

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+docker-compose push;
+
+docker tag catamo/srtv-addressbook_api-gateway registry.heroku.com/srtv-addressbook-catamo-victor/api;
+docker tag catamo/srtv-addressbook_users-service registry.heroku.com/srtv-addressbook-msusers/users_worker;
+docker tag catamo/srtv-addressbook_contacts-service registry.heroku.com/srtv-addressbook-mscontacts/contacts_worker;
+
+docker push registry.heroku.com/srtv-addressbook-catamo-victor/api;
+docker push registry.heroku.com/srtv-addressbook-msusers/users_worker;
+docker push registry.heroku.com/srtv-addressbook-mscontacts/contacts_worker;
+
+heroku container:release api --app srtv-addressbook-catamo-victor
+heroku container:release users_worker --app srtv-addressbook-msusers
+heroku container:release contacts_worker --app srtv-addressbook-mscontacts
