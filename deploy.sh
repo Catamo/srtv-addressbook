a297f82732e4b00557432f018c1f7654e78a17b6
Victor <victor.catamo.rojas@gmail.com>
2019-04-02 01:09:41 -0300

    Testing pushing only one image

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose push
-docker-compose -f docker-compose.heroku.yml push
+
+heroku container:release web --app registry.heroku.com/srtv-addressbook-catamo-victor/web
+
+# docker-compose -f docker-compose.heroku.yml push
 
 # heroku container:release api users_worker contacts_worker
