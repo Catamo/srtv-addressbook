a2315c564d4f8d5096a97dbcfadfe4eae1dedd88
Victor <victor.catamo.rojas@gmail.com>
2019-04-02 11:47:58 -0300

    Only pushing to heroku registry

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose push;
+docker-compose -f docker-compose.heroku.yml build;
 
-ls -R
-cd ./api-gateway
-ls -R
-docker push registry.heroku.com/srtv-addressbook-catamo-victor/web;
-heroku container:release web --app srtv-addressbook-catamo-victor
-
-# docker-compose -f docker-compose.heroku.yml push
+docker-compose -f docker-compose.heroku.yml push;
 
 # heroku container:release api users_worker contacts_worker
