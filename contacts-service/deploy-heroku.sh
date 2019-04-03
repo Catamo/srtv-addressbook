d95be2703c167cf67c26d4c30e405249426d4ee9
Victor <victor.catamo.rojas@gmail.com>
2019-04-02 22:24:00 -0300

    It was tagging api-gateway as users-service. Fixed

--- contacts-service/deploy-heroku.sh
+++ contacts-service/deploy-heroku.sh
@@ -1,7 +1,8 @@
-docker tag catamo/srtv-addressbook_contacts-service registry.heroku.com/srtv-addressbook-mscontacts/worker
+docker tag catamo/srtv-addressbook_contacts-service registry.heroku.com/srtv-addressbook-mscontacts/worker;
 
 # heroku container:push worker --app srtv-addressbook-mscontacts
 
 docker push registry.heroku.com/srtv-addressbook-mscontacts/worker;
 
-heroku container:release worker --app srtv-addressbook-mscontacts
+heroku container:release worker --app srtv-addressbook-mscontacts;
+heroku ps:scale worker=1 --app srtv-addressbook-mscontacts;
--- users-service/deploy-heroku.sh
+++ users-service/deploy-heroku.sh
@@ -1,7 +1,8 @@
-docker tag catamo/srtv-addressbook_api-gateway registry.heroku.com/srtv-addressbook-msusers/worker;
+docker tag catamo/srtv-addressbook_users-service registry.heroku.com/srtv-addressbook-msusers/worker;
 
 # heroku container:push worker --app srtv-addressbook-msusers
 
 docker push registry.heroku.com/srtv-addressbook-msusers/worker;
 
 heroku container:release worker --app srtv-addressbook-msusers;
+heroku ps:scale worker=1 --app srtv-addressbook-msusers;;
