d2e63e08f4fe4d8699d801d223d76b272866e4b0
Victor <victor.catamo.rojas@gmail.com>
2019-04-02 23:08:13 -0300

    Removed semi-colons

--- contacts-service/deploy-heroku.sh
+++ contacts-service/deploy-heroku.sh
@@ -1,8 +1,6 @@
-docker tag catamo/srtv-addressbook_contacts-service registry.heroku.com/srtv-addressbook-mscontacts/worker;
+docker tag catamo/srtv-addressbook_contacts-service registry.heroku.com/srtv-addressbook-mscontacts/worker
 
-# heroku container:push worker --app srtv-addressbook-mscontacts
+docker push registry.heroku.com/srtv-addressbook-mscontacts/worker
 
-docker push registry.heroku.com/srtv-addressbook-mscontacts/worker;
-
-heroku container:release worker --app srtv-addressbook-mscontacts;
-heroku ps:scale worker=1 --app srtv-addressbook-mscontacts;
+heroku container:release worker --app srtv-addressbook-mscontacts
+heroku ps:scale worker=1 --app srtv-addressbook-mscontacts
--- users-service/deploy-heroku.sh
+++ users-service/deploy-heroku.sh
@@ -1,8 +1,6 @@
-docker tag catamo/srtv-addressbook_users-service registry.heroku.com/srtv-addressbook-msusers/worker;
+docker tag catamo/srtv-addressbook_users-service registry.heroku.com/srtv-addressbook-msusers/worker
 
-# heroku container:push worker --app srtv-addressbook-msusers
+docker push registry.heroku.com/srtv-addressbook-msusers/worker
 
-docker push registry.heroku.com/srtv-addressbook-msusers/worker;
-
-heroku container:release worker --app srtv-addressbook-msusers;
-heroku ps:scale worker=1 --app srtv-addressbook-msusers;;
+heroku container:release worker --app srtv-addressbook-msusers
+heroku ps:scale worker=1 --app srtv-addressbook-msusers
